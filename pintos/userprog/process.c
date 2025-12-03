82871175df1a3bfc164b15feda4bc797b53ec74a
Entr0py127 <idjw121611@gmail.com>
2025-12-03 17:06:32 +0900

    feat(vm): m1_ver2 spt copy와 load_segment에서 오류시 free하고 return false하도록 추

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -805,6 +805,7 @@ static bool load_segment(struct file* file, off_t ofs, uint8_t* upage, uint32_t 
         aux->read_bytes = page_read_bytes;
         aux->zero_bytes = page_zero_bytes;
         if (!vm_alloc_page_with_initializer(VM_ANON, upage, writable, lazy_load_segment, aux)) {
+            file_close(aux->file);
             free(aux);
             return false;
         }
